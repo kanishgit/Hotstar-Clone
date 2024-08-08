529f46d50642473e1051f7d44141b60916c367b0
kanishgit <kanishcloud@gmail.com>
2024-08-08 13:49:26 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydealpha" # Replace with your actual S3 bucket name
+    bucket = "kanishs3" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
